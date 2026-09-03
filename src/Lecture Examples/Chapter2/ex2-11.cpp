6d05943e845c3d44ba3de66add188be20c0e5a85
Luke Graten <lukegraten25@gmail.com>
2026-09-03 08:37:46 -0500

    Fix lecture example code to avoid compile errors

--- src/Lecture Examples/Chapter2/ex2-11.cpp
+++ src/Lecture Examples/Chapter2/ex2-11.cpp
@@ -8,7 +8,15 @@ int main()
     const int NO_OF_STUDENTS = 20 ;
     const char BLANK = ' ' ;
 
+
+    // NOTE: Will crash program at runtime since constants are immutable
     //NO_OF_STUDENTS = 21 ;
 
+
+    // Extra cout statements to avoid compiler warnings about unused variables
+    cout << "NOTE: This is extra stuff so the program compiles." << '\n' ;
+    cout << "CONVERSION = " << CONVERSION << '\n' ;
+    cout << "NO_OF_STUDENTS = " << NO_OF_STUDENTS << '\n' ;
+    cout << "BLANK = " << BLANK << '\n' ;
     return 0 ;
 }

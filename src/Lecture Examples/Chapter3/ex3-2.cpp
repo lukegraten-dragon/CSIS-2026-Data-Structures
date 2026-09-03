26cc84c02ef20c4fece4b9cfcc89061395f0a873
Luke Graten <lukegraten25@gmail.com>
2026-09-03 10:11:36 -0500

    Fix lecture code example

--- src/Lecture Examples/Chapter3/ex3-2.cpp
+++ src/Lecture Examples/Chapter3/ex3-2.cpp
@@ -4,12 +4,20 @@ using namespace std ;
  
 int main()   
 {  
+
+    // Declares variables and their types
     int a, b ;
     double z ;
     char ch ;
 
+    // Uses user defined input to assign values to the variables
     cin >> a >> ch >> z ;
     cout << "a = " << a << endl << "ch = " << ch << endl << "z = " << z << endl ;
+
+    // Fix variable b to make the source compilable
+
+    b = 8 ;
+    cout << "b = " << b << endl ;
 
     return 0 ;
 }

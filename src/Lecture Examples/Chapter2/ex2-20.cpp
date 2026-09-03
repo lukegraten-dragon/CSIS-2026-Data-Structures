4251134f8a128068c43c34da63bdc0b4802319a1
Luke Graten <lukegraten25@gmail.com>
2026-09-03 10:29:32 -0500

    Update example with comments

--- src/Lecture Examples/Chapter2/ex2-20.cpp
+++ src/Lecture Examples/Chapter2/ex2-20.cpp
@@ -4,7 +4,7 @@ using namespace std ;
  
 int main()   
 {  
-
+    // Declared variables
     int a ;
     int b ;
 
@@ -14,6 +14,7 @@ int main()
     cout << "a = " << a << endl << "b = " << b << endl ;
 
     a = 5 ;
+    // This one is different. 5 will be added with the 2 and then variable a will be increased separately. Resulting in potentially unintended behavior :(
     b = 2 + (a++) ;
     cout << "a = " << a << endl << "b = " << b << endl ;
 
